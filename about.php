1a4d53b65984a07d76f3e851199f9db94a521f5f
unknown <lorenzojanrommel@gmail.com>
2018-07-19 22:32:31 +0800

    about

--- about.php
+++ about.php
@@ -5,7 +5,61 @@
 	function display_content(){
 		?>
 			<div class="container">
-				<h1>About</h1>
+				<h3 class="about-us-h3">About Us</h3>
+				<p class="about-us-p">Boarding house and Dormitories finder is a new method of accessing information of the business by integrating the user interface design on the website, not only browsing content but also to process information and transaction. This website eliminate the problems being encountered by the customer for finder a boarding house and dormitories such as wasting time to search for available boarding house and dormitories in manual process especially for people who came from far places in Tarlac Province, especially in the municipalities.</p>
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="member-container">
+							<h3 class="text-center our-team">Our Team</h3>
+							<div class="row">
+								<div class="col-sm-12 col-md-6 col-lg-6">
+									<div class="member-images">
+										<img class="member-img" src="assets/img/member/wes.png">
+									</div>
+									<h5 class="member-name">Wesley Sebastian</h5>
+									<div class="member-details">
+										<p class="member-details-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+									</div>
+								</div>
+								<div class="col-sm-12 col-md-6 col-lg-6">
+									<div class="member-images">
+										<img class="member-img" src="assets/img/member/norvin.png">
+									</div>
+									<h5 class="member-name">Norvin Mercado</h5>
+									<div class="member-details">
+										<p class="member-details-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+									</div>
+								</div>
+								<div class="col-sm-12 col-md-6 col-lg-6">
+									<div class="member-images">
+										<img class="member-img" src="assets/img/member/jeffrey.png">
+									</div>
+									<h5 class="member-name">Jeffrey Lee Lino</h5>
+									<div class="member-details">
+										<p class="member-details-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+									</div>
+								</div>
+								<div class="col-sm-12 col-md-6 col-lg-6">
+									<div class="member-images">
+										<img class="member-img" src="assets/img/member/iohan.png">
+									</div>
+									<h5 class="member-name">Iohan Mendoza</h5>
+									<div class="member-details">
+										<p class="member-details-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>	
+				</div>
 			</div>
 		<?php
 	}
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 			<div class="par1-index">
 				<div class="container">
 					<div class="title-header">
-						<h1 class="title-header text-uppercase">Boarding House & Dormitories Finder</h1>
+						<h1 class="title-header text-uppercase">Boarding House <span class="and">&</span> <span class="dorm">Dormitories Finder</span></h1>
 					</div>
 				</div>
 			</div>
