b2fdfda300e922be0fc91df8608d9bccaec61f47
unknown <lorenzojanrommel@gmail.com>
2018-09-04 01:42:30 +0800

    mapv2

--- landlord/owner_dashboard.php
+++ landlord/owner_dashboard.php
@@ -46,10 +46,11 @@
 				  		<p><b>Description:</b> <?php echo $house_description ?></p>
 				  	</div>
 				  </div>
-				  <div class="map">
+				  <div class="map" onload="initMap">
 				  	<h5>Map</h5>
 				  	<div id="map-display">
-				  		
+				  		<!-- <p id="lat">15.69</p>
+				  		<p id="lng">120.41</p> -->
 				  	</div>
 				  </div>
 			  </div>
@@ -72,29 +73,53 @@
 	require 'create_bhod_modal.php';
 ?>
 	<script type="text/javascript">
-	$(document).ready(function() {
-		var location = $('span#location').text();
-		console.log(location);
-		$.ajax({
-			method : 'GET',
-			url : 'https://maps.googleapis.com/maps/api/geocode/json',
-			data : {
-				address : location,
-				key : 'AIzaSyC13PAMU-yO3K-JW2VmMmuZZ2YalWmc7GQ'
-			},
-			success : function(data){
-				// log full response
-				console.log(data);
-				// Formatted Address
-				var formattedAddress = data.results[0].formatted_address;
-				var formattedAddressOutput = `
-
-
-				`;
-			},
-			error : function(data){
-				console.log(data);
-			}
+//	$(document).ready(function() {
+//		var location = $('span#location').text();
+//		console.log(location);
+//		$.ajax({
+//			method : 'GET',
+//			url : 'https://maps.googleapis.com/maps/api/geocode/json',
+//			data : {
+//				address : location,
+//				key : 'AIzaSyC13PAMU-yO3K-JW2VmMmuZZ2YalWmc7GQ'
+//			},
+//			success : function(data){
+//				// log full response
+//				console.log(data);
+//				// Formatted Address
+//				var formattedAddress = data.results[0].formatted_address;
+//				var lat = data.results[0].geometry.location.lat;
+//				var lng = data.results[0].geometry.location.lng;
+//				console.log(lat);
+//				console.log(lng);
+//			},
+//			error : function(data){
+//				console.log(data);
+//			}
+//		})
+//	}) 
+	function initMap() {
+		// var getLat = $('#lat').text();
+		// var getLng = $('#lng').text();
+		// console.log(getLng);
+		// console.log(getLat);
+		// Map Option
+		var options = {
+			zoom : 11,
+			center : {
+					lat: 15.4755,
+					lng: 120.5963}
+		}
+		// New Map
+		var map = new google.maps.Map(document.getElementById('map-display'), options);
+		// Add Market
+		// var logo = '../assets/img/bhod_logo.png';
+		var marker = new google.maps.Marker({
+			position: {lat:15.4755, lng:120.5963},
+			map : map,
 		})
-	})
+	}
 	</script>
+	<script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC13PAMU-yO3K-JW2VmMmuZZ2YalWmc7GQ&callback=initMap">
+    </script>
