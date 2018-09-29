2cd24ba27319c723336b773bc0087ba028ac147f
unknown <lorenzojanrommel@gmail.com>
2018-09-29 22:23:25 +0800

    house list

--- admin/house.php
+++ admin/house.php
@@ -6,7 +6,7 @@
 		require '../condb.php';
 		?>
 
-		<div class="container mt-3 mb-5">
+		<div class="container mt-3 mb-5 pd-5">
 			  <ul class="nav nav-tabs" role="tablist">
 			  <li class="nav-item">
 			    <a class="nav-link active" data-toggle="tab" href="#boarding_house" role="tab" aria-controls="boarding_house">Boarding House</a>
@@ -30,7 +30,7 @@
 			  		<div class="container">
 			  			<div class="row">
 			  			<div class="col-sm-12 mt-2">
-			  			<table class="boarding-house-list" id="boarding-house-list" class="table table-hover">
+			  			<table class="boarding-house-list w-auto table-responsive" id="boarding-house-list" class="table table-hover">
 			  				<thead>
 			  					<tr>
 			  						<th>House Name</th>
@@ -86,8 +86,6 @@
 							<tr>
 								<th>House Name</th>
 								<th>Owner</th>
-								<th>BIR Permit</th>
-								<th>Mayors Permit</th>
 								<th>Business License Permit</th>
 								<th>Status</th>
 							</tr>
@@ -110,12 +108,6 @@
 									echo $user_fname,str_repeat('&nbsp;', 1),substr($user_mname, 0, 1).".".str_repeat('&nbsp;', 1),ucfirst($user_lname);
 									?>
 								</td>
-								<!-- View BIR -->
-								<td class="text-center"><button type="button" class="btn btn-info view-bir" data-id=<?php echo $house_id?> data-toggle="modal" data-target="#view-bir-picture">View</button></td>
-								<!-- End view bir -->
-								<!-- View mayors permit -->
-								<td class="text-center"><button type="button" class="btn btn-success view-mayors-permit" data-id="<?php echo $house_id ?>" data-toggle="modal" data-target="#view-mayors-permit-picture">View</button></td>
-								<!-- End view mayors permit -->
 								<!-- View business plate -->
 								<td class="text-center"><button type="button" class="btn btn-warning view-business-plate" data-id="<?php echo $house_id ?>" data-toggle="modal" data-target="#view-business-plate">View</button></td>
 								<!-- End view Business plate -->
@@ -141,8 +133,6 @@
 			  						<tr>
 			  							<th>House Name</th>
 			  							<th>Owner</th>
-			  							<th>BIR Permit</th>
-			  							<th>Mayors Permit</th>
 			  							<th>Business License Permit</th>
 			  							<th>Status</th>
 			  						</tr>
@@ -166,14 +156,8 @@
 			  								extract($row1);
 			  								echo $user_fname,str_repeat('&nbsp;', 1),substr($user_mname, 0, 1).".".str_repeat('&nbsp;', 1),ucfirst($user_lname);?></td>
 			  							<!-- End of boarding house owner -->
-			  							<!-- Boarding House BIR permit -->
-			  							<td><button type="button" class="btn btn-info view-bir" data-id=<?php echo $house_id?> data-toggle="modal" data-target="#view-bir-picture">View</button></td>
-			  							<!-- End of boarding house permit -->
-			  							<!-- Boarding house Mayors Permit -->
-			  							<td><button type="button" class="btn btn-success view-mayors-permit" data-id="<?php echo $house_id ?>" data-toggle="modal" data-target="#view-mayors-permit-picture">View</button></td>
-			  							<!-- End of boarding house permit -->
 			  							<!-- Business Plate -->
-			  							<td><button type="button" class="btn btn-warning view-business-plate" data-id="<?php echo $house_id ?>" data-toggle="modal" data-target="#view-business-plate">View</button></td>
+			  							<td class="text-center"><button type="button" class="btn btn-warning view-business-plate" data-id="<?php echo $house_id ?>" data-toggle="modal" data-target="#view-business-plate">View</button></td>
 			  							<!-- End business Plate -->
 			  							<!-- Boarding house status -->
 			  							<td>
@@ -201,8 +185,6 @@
 			  				<tr>
 			  					<th>House Name</th>
 			  					<th>Owner</th>
-			  					<th>BIR Permit</th>
-			  					<th>Mayors Permit</th>
 			  					<th>Business License Permit</th>
 			  					<th>Status</th>
 			  				</tr>
@@ -224,13 +206,6 @@
 			  					$all_house_row = mysqli_fetch_assoc($user);
 			  					extract($all_house_row);
 			  					echo $user_fname,str_repeat('&nbsp;', 1),substr($user_mname, 0, 1).".".str_repeat('&nbsp;', 1),ucfirst($user_lname);?></td>
-			  				<!-- End of  house owner -->
-			  				<!--  House BIR permit -->
-			  				<td class="text-center"><button type="button" class="btn btn-info view-bir" data-id=<?php echo $house_id?> data-toggle="modal" data-target="#view-bir-picture">View</button></td>
-			  				<!-- End of  house permit -->
-			  				<!--  house Mayors Permit -->
-			  				<td class="text-center"><button type="button" class="btn btn-success view-mayors-permit" data-id="<?php echo $house_id ?>" data-toggle="modal" data-target="#view-mayors-permit-picture">View</button></td>
-			  				<!-- End of  house permit -->
 			  				<!-- Business Plate -->
 			  				<td class="text-center"><button type="button" class="btn btn-warning view-business-plate" data-id="<?php echo $house_id ?>" data-toggle="modal" data-target="#view-business-plate">View</button></td>
 			  				<!-- End business Plate -->
--- admin/view_business_plate_modal_body.php
+++ admin/view_business_plate_modal_body.php
@@ -7,7 +7,13 @@
 	extract($row);
 	?>
 	<div class="container">
-		<img class="img-fluid" src="../<?php echo $house_blpp ?>" alt="<?php echo $house_name ?>">
+		<h3 class="text-center"><?php if ($house_category_id = 'Boarding House') {
+			echo "Boarding House";
+		}elseif($house_category_id = 'Dormitory'){
+			echo "Dormitory";
+		}?></h3>
+		<h6 class="text-center">House Type</h6>
+		<img class="img-fluid" src="<?php echo $house_blpp ?>" alt="<?php echo $house_name ?>">
 	</div>
 		<div class="modal-footer">
 	  		<button type="button" class="btn btn-primary ml-1" data-dismiss="modal">Close</button>
