8d3d265de25d3068cf3c28fd1b0bcf943909cf53
unknown <lorenzojanrommel@gmail.com>
2018-10-02 05:53:41 +0800

    view reservation at customer

--- customer/view_profile.php
+++ customer/view_profile.php
@@ -39,6 +39,9 @@
 		                                <li class="nav-item">
 		                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
 		                                </li>
+		                                <li class="nav-item">
+		                                  <a class="nav-link" data-toggle="tab" href="#reservation" role="tab" aria-controls="reservation">Reservation</a>
+		                                </li>
 		                            </ul>
 		                        </div>
 		                    </div>
@@ -112,6 +115,94 @@
 		                                                <p><?php echo $user_birthdate; ?></p>
 		                                            </div>
 		                                        </div>
+		                            </div>
+		                            <div class="tab-pane fade" id="reservation" role="tabpanel" aria-labelledby="home-tab">
+		                             <div class="row">
+			  							<div class="col-sm-12 mt-2 table-responsive">
+			  							<table class="customer-list w-auto table" id="customer-list" class="table table-hover">
+			  								<thead>
+			  									<tr>
+			  										<th>House Name</th>
+			  										<th>Room Number</th>
+			  										<th>House Owner</th>
+			  										<th>Room Price</th>
+			  										<th>Status</th>
+			  										<th>Action</td>
+			  									</tr>
+			  								</thead>
+			  								<tbody>
+			  									<?php
+			  									$reservation = "SELECT * FROM reservations WHERE customer_id = '$id'";
+			  									$reservation_results = mysqli_query($conn, $reservation);
+			  									$reservation_row = mysqli_fetch_assoc($reservation_results);
+			  									extract($reservation_row);
+			  									?>
+			  									<tr>
+			  										<td class="text-center"><?php 
+			  										$house = "SELECT * FROM `houses` JOIN rooms ON (houses.house_id = rooms.house_id) JOIN reservations ON (rooms.room_id = reservations.room_id) WHERE owner_id = '$owner_id'";
+			  										$house_results = mysqli_query($conn, $house);
+			  										$house_row = mysqli_fetch_assoc($house_results);
+			  										extract($house_row);
+			  										echo $house_name;
+			  										?></td>
+			  										<td class="text-center">
+			  											<?php
+			  											$room_number = "SELECT * FROM rooms WHERE room_id = '$room_id'";
+			  											$room_results = mysqli_query($conn, $room_number);
+			  											$room_row = mysqli_fetch_assoc($room_results);
+			  											extract($room_row);
+			  											echo $room_number;
+			  											?>
+			  										</td>
+			  										<td>
+			  											<?php
+			  											$owner = "SELECT * FROM users WHERE id = '$owner_id'";
+			  											$owner_results = mysqli_query($conn, $owner);
+			  											$owner_row = mysqli_fetch_assoc($owner_results);
+			  											extract($owner_row);
+			  											echo $user_fname,str_repeat('&nbsp;', 1),substr($user_mname, 0, 1).".".str_repeat('&nbsp;', 1),ucfirst($user_lname);
+			  											?>
+			  										</td>
+			  										<td class="text-center"><?php
+			  											echo $room_price;
+			  										?></td>
+			  										<?php
+			  										if ($reservation_status == '4') {
+			  											?>
+			  											<td class="text-center">Approved</td>
+			  											<?php
+			  										}elseif($reservation_status == '3'){
+			  											?>
+			  											<td class="text-center">Pending</td>
+			  											<?php
+			  										}elseif ($reservation_status == '5') {
+			  											?>
+			  											<td class="text-center">Denied</td>
+			  											<?php
+			  										}
+			  										?>
+			  										<td>
+			  											<?php
+			  											if ($reservation_status == '5') {
+			  												?>
+			  												<button type="button" class="btn btn-outline-danger">Delete</button>
+			  												<?php
+			  											}elseif($reservation_status == '3'){
+			  												?>
+			  												<button type="button" class="btn btn-outline-warning">Cancel</button>
+			  												<?php
+			  											}else{
+			  												?>
+			  												<button type="button" class="btn btn-success">Approved</button>
+			  												<?php
+			  											}
+			  											?>
+			  										</td>
+			  									</tr>
+			  								</tbody>
+			  							</table>
+			  						</div>
+			  						</div>
 		                            </div>
 		                        </div>
 		                    </div>
